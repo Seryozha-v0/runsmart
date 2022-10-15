d2211c99f976da1b573189f654556dfe6c4325f8
Seryozha <arzumanyan.seroj@mail.ru>
2022-10-15 20:25:53 +0300

    add tiny carousel

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,26 @@
-$(document).ready(function () {
-    $('.carousel__inner').slick({
-        speed: 800,
-        adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>',
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                    dots: true,
-                    arrows: false
-                }
-            }
-        ]
-    });
+document.addEventListener("DOMContentLoaded", function(event) {
+    
+    let carousel = tns({
+        container: '.carousel__inner',
+        items: 1,
+        autoplay: false,
+        nav: true,
+        navPosition: 'bottom',
+        controls: false,
+        mouseDrag: true,
+        responsive: {
+            992: {
+                nav: false,
+            },
+        }
+      });
+
+    document.querySelector('.carousel_prev').onclick = function () {
+        carousel.goTo('prev');
+    };
+
+    document.querySelector('.carousel_next').onclick = function () {
+        carousel.goTo('next');  
+    };
+
 });
