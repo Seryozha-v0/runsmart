c275116898b91ca813982c02815c5d998f4a54a2
Seryozha <arzumanyan.seroj@mail.ru>
2022-10-17 22:35:33 +0300

    Create catalog section

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,5 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    
+document.addEventListener('DOMContentLoaded', function (event) {
+
     let carousel = tns({
         container: '.carousel__inner',
         items: 1,
@@ -13,14 +13,33 @@ document.addEventListener("DOMContentLoaded", function(event) {
                 nav: false,
             },
         }
-      });
+    });
 
-    document.querySelector('.carousel_prev').onclick = function () {
+    document.querySelector('.carousel_prev').addEventListener('click', function () {
         carousel.goTo('prev');
-    };
+    });
 
-    document.querySelector('.carousel_next').onclick = function () {
-        carousel.goTo('next');  
-    };
+    document.querySelector('.carousel_next').addEventListener('click', function () {
+        carousel.goTo('next');
+    });
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+        $(this)
+            .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+            .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    function toggleSlide(item) {
+        $(item).each(function (i) {
+            $(this).on('click', function (e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+        })
+    }
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
 
 });
