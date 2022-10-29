59d0a8af1483cdcdb0e01f2bb026effea4fca9dc
Seryozha <arzumanyan.seroj@mail.ru>
2022-10-29 19:56:30 +0300

    Created send emails

--- src/js/script.js
+++ src/js/script.js
@@ -86,4 +86,21 @@ document.addEventListener('DOMContentLoaded', function (event) {
 
     $('input[name=phone]').mask("+7 (999) 999-99-99");
 
+    
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut('fast');
+            $('.overlay, #thanks').fadeIn('fast')
+            $('form').trigger('reset');
+        });
+
+        return false;
+    });
+
 });
